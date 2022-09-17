33155ce08b2b86ccf950311648aecc205076ff9f
saruar-hosen <saruarhosen525@gmail.com>
2022-09-17 20:16:31 +0600

    Update Code

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,21 +29,37 @@
     autoplay: true,
     autoplaySpeed: 2000,
     autoplayTimeout: 5000,
-    margin: 20,
+    margin: 12,
     nav: false,
     dots: false,
     mouseDrag: true,
-    stagePadding: 50,
+    stagePadding: 35,
     items: 1,
     responsive: {
+      370: {
+        stagePadding: 51,
+      },
+      391: {
+        stagePadding: 40,
+      },
+      440: {
+        stagePadding: 60,
+        margin: 20,
+      },
       768: {
-        items: 3,
+        items: 2,
+        stagePadding: 60,
+        margin: 20,
       },
       992: {
-        items: 4,
+        items: 3,
+        stagePadding: 60,
+        margin: 20,
       },
       1200: {
         items: 4,
+        stagePadding: 60,
+        margin: 20,
       },
     },
   });
